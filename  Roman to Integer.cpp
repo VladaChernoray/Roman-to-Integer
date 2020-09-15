6e8e480bdff3d18b59f89edb40601c972a4bbf64
VladaChernoray <57546413+VladaChernoray@users.noreply.github.com>
2020-09-15 23:04:58 +0300

    Add files via upload

---
+++  Roman to Integer.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.empty())
+            return 0;
+
+        unordered_map<char, int> map({ {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},  {'C', 100}, {'D', 500}, {'M', 1000} });
+
+        int val = 0;
+        for (int i = 0; i < s.size(); i++) {
+            val += map[s[i]];
+            if (i > 0 && map[s[i]] > map[s[i - 1]])
+                val -= 2 * map[s[i - 1]];
+        }
+        return val;
+    }
+};
